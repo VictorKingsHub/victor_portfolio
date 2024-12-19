942b4d1aafc14a15f6456c4ee0a63088a7333723
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-12-19 11:56:39 +0100

    counter added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import AccordionMenu from "@/ui/about/AccordionMenu/Accordion";
-import CountUpSection from "@/ui/about/countUp/CountUp";
 import HeroSection from "@/ui/about/hero/Hero";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="w-screen flex flex-col">
       <HeroSection />
-      {/* <CountUpSection /> */}
       <AccordionMenu />
     </main>
   );
